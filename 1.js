649a266e4e29707a23269e9f41dc09e4a45a97ac
nguyenhoaitran-gr <trannguyen.hcmus@gmail.com>
2018-04-16 10:56:45 +0700

    Xong khoi education and experience

--- 1.js
+++ 1.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	
+	$(window).scroll(function(event) {
+		/* Act on the event */
+		var vitribody = $('html,body').scrollTop();
+		console.log(vitribody);
+		if(vitribody >= 15)
+		{
+			$('.menuphai').addClass('scroll');
+		}
+		else{
+			$('.menuphai').removeClass('scroll');
+		}
+	});;
 });
