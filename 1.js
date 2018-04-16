79c90fea4cc3c320e24c3b53aa5209a5b0f8005f
nguyenhoaitran-gr <trannguyen.hcmus@gmail.com>
2018-04-17 00:39:08 +0700

    js scroll cho man hinh va scrollspy

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$(window).scroll(function(event) {
-		/* Act on the event */
+	$(window).scroll(function() {
+		/* Act on the  */
 		var vitribody = $('html,body').scrollTop();
 		console.log(vitribody);
 		if(vitribody >= 15)
@@ -11,8 +11,37 @@ $(document).ready(function() {
 			$('.menuphai').removeClass('scroll');
 		}
 	});
-
+	$('body').scrollspy({
+		target:'#menutren'});
 	$('.khoi-product').isotope({
 		itemSelector:'li'
 	});
+
+	// js cho click chuot
+	$('.navlink.nha').click(function() {
+		$('html,body').animate({scrollTop: $('#_home').offset().top},1000);
+		
+	});
+	$('.navlink.toi').click(function() {
+		$('html,body').animate({scrollTop: $('#_about').offset().top},1000);
+		
+	});
+	$('.navlink.gd').click(function() {
+		$('html,body').animate({scrollTop: $('#_edu').offset().top},1000);
+		
+	});
+	$('.navlink.manh').click(function() {
+		$('html,body').animate({scrollTop: $('#_strength').offset().top},1000);
+		
+	});
+	$('.navlink.sothich').click(function() {
+		$('html,body').animate({scrollTop: $('#_hobbies').offset().top},1000);
+		
+	});
+	$('.navlink.lienlac').click(function() {
+		$('html,body').animate({scrollTop: $('#_fb').offset().top},1000);
+		
+	});
+	
+
 });
